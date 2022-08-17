cd3b28ee5d38179ba03276fadbadd4e010961711
Mrbabet <108229726+Mrbabet@users.noreply.github.com>
2022-08-17 20:20:26 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const menuBtn = document.querySelector(".menu-btn");
+const hiddenMenu = document.querySelector(".menu-mobile");
+
+let menuOpen = false;
+menuBtn.addEventListener("click", () => {
+  if (!menuOpen) {
+    menuBtn.classList.add("open");
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove("open");
+    menuOpen = false;
+  }
+});
+menuBtn.addEventListener("click", function () {
+  hiddenMenu.classList.toggle("open");
+});
